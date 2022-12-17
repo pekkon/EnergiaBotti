feff847fd39e0387252489b0867afeaa04a6ba34
Pekko <pekko.niemi@gmail.com>
2022-12-17 23:20:13 +0200

    Cleaned up main function from debugging stuff

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -11,9 +11,9 @@ def lambda_handler(event, context):
     print("hello")
     # Set to false if ran on AWS
     debugging_mode = True
-    #wind_tweets(debugging_mode)
-    #demand_tweets(debugging_mode)
-    #daily_price_graph(debugging_mode)
+    wind_tweets(debugging_mode)
+    demand_tweets(debugging_mode)
+    daily_price_graph(debugging_mode)
 
 
     return {
@@ -24,21 +24,3 @@ def lambda_handler(event, context):
 
 #lambda_handler(None, None)
 
-import pandas as pd
-from entsoe import EntsoePandasClient
-import os
-
-token = os.environ['ENTSO_TOKEN']
-
-client = EntsoePandasClient(api_key=token)
-prices = []
-areas = ['FI', 'SE_1', 'SE_3', 'NO_2', 'EE', 'DE_LU']
-
-for area in areas:
-    serie = client.query_day_ahead_prices(area, start=pd.Timestamp("2021-01-01 00:00", tz='Europe/Helsinki'),
-                                          end=pd.Timestamp("2022-12-14 23:59", tz='Europe/Helsinki'))
-    serie.index = serie.index.strftime("%d.%m.%Y %H:%M")
-    prices.append(serie.reset_index(drop=True))
-areas = ['FI', 'SE1', 'SE3', 'NO2', 'EE', 'DE']
-df = pd.DataFrame(prices, columns=areas, index=serie.index)
-df.to_excel('price_data.xlsx')
